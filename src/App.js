0aacb927e2fb54a2f89d12c9e30e34f81d1ccdab
djavoa99 <djavoa99@naver.com>
2022-11-04 15:07:28 +0900

    Day4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 const App = () => {
-  return <Say />
+  return <EventPractice />
 }
 // 컴포넌트 내부는 하나의 DOM 트리 구조로 이루어져야 한다는 규칙이 있다.
 export default App;
