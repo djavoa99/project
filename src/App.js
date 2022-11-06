e2f518709eed7f5f7f7a4643e0b597e2cb21fc6a
djavoa99 <djavoa99@naver.com>
2022-11-06 14:11:56 +0900

    Day6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import { Component } from 'react';
-import ScrollBox from './ScrollBox.js';
+import IterationSample from './IterationSample.js';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
-        <button onClick={() => this.ScrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
+      <IterationSample />
     )
   }
 }
