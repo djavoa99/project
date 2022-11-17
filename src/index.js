13deac01b390b8c2d3d720c94f280d5a2d85aa7c
djavoa99 <djavoa99@naver.com>
2022-11-18 07:37:48 +0900

    Day18

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import ReactDOM from 'react-dom';
-import {legacy_createStore as createStore} from "redux"
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
-import rootReducer from './modules';
+import * as serviceWorker from './serviceWorker';
+import rootReducer, { rootSaga } from './modules';
+// import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
+
+serviceWorker.unregister();
