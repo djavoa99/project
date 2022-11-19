7e8ad7349092643716affaca9358005837def5b5
djavoa99 <djavoa99@naver.com>
2022-11-19 10:15:41 +0900

    Day19

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import CounterContainer from './containers/CounterContainer';
+import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe.js'), {
+  fallback: <div>loading...</div>
+});
 
-const App = () => {
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
+  };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
-    <div>
-      <CounterContainer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+        {visible && <SplitMe />}
+      </header>
     </div>
   );
-};
+}
 
 export default App;
